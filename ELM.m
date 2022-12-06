61f503ad3bbdc3f6aaf42de5c94ee42e6c94faf9
HuseyinPARMAKSIZ <47844849+HuseyinPARMAKSIZ@users.noreply.github.com>
2022-12-06 08:35:47 +0300

    Update ELM.m

--- ELM.m
+++ ELM.m
@@ -1,228 +1,145 @@
 All codes are in the testing phase yet!!!
-function  [TrainingTime, TestingTime, TrainingAccuracy, TestingAccuracy]= ELM(TrainingData_File, TestingData_File, Elm_Type, NumberofHiddenNeurons, ActivationFunction,C,ite)
-% Usage: elm(TrainingData_File, TestingData_File, Elm_Type, NumberofHiddenNeurons, ActivationFunction)
-% OR:    [TrainingTime, TestingTime, TrainingAccuracy, TestingAccuracy] = elm(TrainingData_File, TestingData_File, Elm_Type, NumberofHiddenNeurons, ActivationFunction)
-%
-% Input:
-% TrainingData_File     - Filename of training data set
-% TestingData_File      - Filename of testing data set
-% Elm_Type              - 0 for regression; 1 for (both binary and multi-classes) classification
-% NumberofHiddenNeurons - Number of hidden neurons assigned to the ELM
-% ActivationFunction    - Type of activation function:
-%                           'sig' for Sigmoidal function
-%                           'sin' for Sine function
-%                           'hardlim' for Hardlim function
-%                           'tribas' for Triangular basis function
-%                           'radbas' for Radial basis function (for additive type of SLFNs instead of RBF type of SLFNs)
-%
-% Output: 
-% TrainingTime          - Time (seconds) spent on training ELM
-% TestingTime           - Time (seconds) spent on predicting ALL testing data
-% TrainingAccuracy      - Training accuracy: 
-%                           RMSE for regression or correct classification rate for classification
-% TestingAccuracy       - Testing accuracy: 
-%                           RMSE for regression or correct classification rate for classification
-%
-% MULTI-CLASSE CLASSIFICATION: NUMBER OF OUTPUT NEURONS WILL BE AUTOMATICALLY SET EQUAL TO NUMBER OF CLASSES
-% FOR EXAMPLE, if there are 7 classes in all, there will have 7 output
-% neurons; neuron 5 has the highest output means input belongs to 5-th class
-%
-% Sample1 regression: [TrainingTime, TestingTime, TrainingAccuracy, TestingAccuracy] = elm('sinc_train', 'sinc_test', 0, 20, 'sig')
-% Sample2 classification: elm('diabetes_train', 'diabetes_test', 1, 20, 'sig')
-%
-    %%%%    Authors:    MR QIN-YU ZHU AND DR GUANG-BIN HUANG
-    %%%%    NANYANG TECHNOLOGICAL UNIVERSITY, SINGAPORE
-%%%%%%%%%%% Macro definition
-REGRESSION=0;
-CLASSIFIER=1;
-ite=25;
-i=1;
-%%%%%%%%%%% Load training dataset
-train_data=TrainingData_File;
-clear TrainingData_File;
-T=train_data(:,1)';
-P=train_data(:,2:size(train_data,2))';
-clear train_data;                                   %   Release raw training data array
 
-%%%%%%%%%%% Load testing dataset
-test_data=TestingData_File;
-clear TestingData_File;
-TV.T=test_data(:,1)';
-TV.P=test_data(:,2:size(test_data,2))';
-clear test_data;                                    %   Release raw testing data array
 
-NumberofTrainingData=size(P,2);
-NumberofTestingData=size(TV.P,2);
-NumberofInputNeurons=size(P,1);
+%% Original  Authors:    MR QIN-YU ZHU AND DR GUANG-BIN HUANG - NANYANG TECHNOLOGICAL UNIVERSITY, SINGAPORE %%
+%% Edited by me last quarter of 2022 (ELM for Radio Frequency Fingerprint Classification Problems.)
 
-while i<=ite
+clear all; close all; clc; warning off
+%------------------Loading datasets ----------------% 
+%-----Load Ready-RF dataset -----------------------------%
+load('Ready6Ftrvete.mat'); % load('Ready9Ftrvete.mat');
+Yd_train=train_data(:,1)'; X=train_data(:,2:size(train_data,2))'; X=zscore(X);
+Yd_test=test_data(:,1)';  Xt=test_data(:,2:size(test_data,2))'; Xt=zscore(Xt);
+%-----Definitions--------%
+Elm_Type = 1; REGRESSION=0; CLASSIFIER=1; ite=1; C=max(eig(X'*X)); % regularization factor                              
+N=size(X,2); %training data size
+Nt=size(Xt,2); %testing data size
+Nin=size(X,1);% input neurons size
+Nhn=100; % hidden neurons size
+ActivationFunction='tansig';
+DenSay=5; % iteration size
+
 if Elm_Type~=REGRESSION
-    %%%%%%%%%%%% Preprocessing the data of classification
-    sorted_target=sort(cat(2,T,TV.T),2);
-    label=zeros(1,1);                               %   Find and save in 'label' class label from training and testing data sets
+    %----- Preprocessing the data of classification
+    sorted_target=sort(cat(2,Yd_train,Yd_test),2); % 1*3985 bütün sınıflar sıralı yan yana 
+    label=zeros(1,1);  % Find and save in 'label' class label from training and testing data sets
     label(1,1)=sorted_target(1,1);
     j=1;
-    for i = 2:(NumberofTrainingData+NumberofTestingData)
+    for i = 2:(N+Nt)
         if sorted_target(1,i) ~= label(1,j)
             j=j+1;
             label(1,j) = sorted_target(1,i);
         end
     end
     number_class=j;
-    NumberofOutputNeurons=number_class;
+    Non=number_class; %output neurons size, class size
        
-    %%%%%%%%%% Processing the targets of training
-    temp_T=zeros(NumberofOutputNeurons, NumberofTrainingData);
-    for i = 1:NumberofTrainingData
+    %----- Preprocessing the targets of training
+    temp_T=zeros(Non, N);
+    for i = 1:N
         for j = 1:number_class
-            if label(1,j) == T(1,i)
+            if label(1,j) == Yd_train(1,i)
                 break; 
             end
         end
         temp_T(j,i)=1;
     end
-    T=temp_T*2-1;
+    Yd_train=temp_T*2-1; % 4*2378 hangi sınıfa aitse 1 diğerleri -1
 
-    %%%%%%%%%% Processing the targets of testing
-    temp_TV_T=zeros(NumberofOutputNeurons, NumberofTestingData);
-    for i = 1:NumberofTestingData
+    %----- Preprocessing the targets of testing
+    temp_TV_T=zeros(Non, Nt);
+    for i = 1:Nt
         for j = 1:number_class
-            if label(1,j) == TV.T(1,i)
+            if label(1,j) == Yd_test(1,i)
                 break; 
             end
         end
         temp_TV_T(j,i)=1;
     end
-    TV.T=temp_TV_T*2-1;
+    Yd_test=temp_TV_T*2-1;  % 4*1019 hangi sınıfa aitse 1 diğerleri -1 
 
-end                                                 %   end if of Elm_Type
+end    %   end if of Elm_Type
 
-%%%%%%%%%%% Calculate weights & biases
+while ite<=DenSay
+%-- Calculate weights & biases
 start_time_train=tic; %cputime;
 
-%%%%%%%%%%% Random generate input weights InputWeight (w_i) and biases BiasofHiddenNeurons (b_i) of hidden neurons
-InputWeight=rand(NumberofHiddenNeurons,NumberofInputNeurons)*2-1;
-BiasofHiddenNeurons=rand(NumberofHiddenNeurons,1);
-tempH=InputWeight*P;
-clear P;                                            %   Release input of training data 
-ind=ones(1,NumberofTrainingData);
-BiasMatrix=BiasofHiddenNeurons(:,ind);              %   Extend the bias matrix BiasofHiddenNeurons to match the demention of H
+%%%%%%%%%%% Random generate input weights InputWeight (w_i) and biases Bhn (b_i) of hidden neurons
+InputWeight=rand(Nhn,Nin)*2-1;
+Bhn=rand(Nhn,1);
+tempH=InputWeight*X;
+ind=ones(1,N);
+BiasMatrix=Bhn(:,ind);    %   Extend the bias matrix Bhn to match the demention of H
 tempH=tempH+BiasMatrix;
 
 %%%%%%%%%%% Calculate hidden neuron output matrix H
 switch lower(ActivationFunction)
-    case {'sig','sigmoid'}
+    case {'logsig','sigmoid'}
         %%%%%%%% Sigmoid 
         H = 1 ./ (1 + exp(-tempH));
-    case {'logsig','logsigmoid'}
-        %%%%%%%% Sigmoid 
-        H = 1 ./ (1 + exp(-tempH));
-    case {'sin','sine'}
-        %%%%%%%% Sine
-        H = sin(tempH);    
-    case {'hardlim'}
-        %%%%%%%% Hard Limit
-        H = double(hardlim(tempH));
-    case {'tribas'}
-        %%%%%%%% Triangular basis function
-        H = tribas(tempH);
-    case {'radbas'}
+    case {'tansig'}
         %%%%%%%% Radial basis function
-        H = radbas(tempH);
-         case {'tansig'}
-        %%%%%%%% Radial basis function
-        H = tansig(tempH);
-        %%%%%%%% More activation functions can be added here                
+        H = tansig(tempH);               
 end
-clear tempH;                                        %   Release the temparary array for calculation of hidden neuron output matrix H
+clear tempH;  %   Release the temporary hidden neuron output matrix H
+%--- Calculate output weights OutputWeight (beta_i)
+%OutputWeight=pinv(H') * T';      
+%OutputWeight=inv(eye(size(H,1))/C+H * H') * H * T';   % faster method 1 
+OutputWeight=(eye(size(H,1))/C+H * H') \ H * Yd_train';      % faster method 2
+TrainigTime=toc(start_time_train);
+timeTr(ite)=TrainigTime;
 
-%%%%%%%%%%% Calculate output weights OutputWeight (beta_i)
-%OutputWeight=pinv(H') * T';                        % implementation without regularization factor //refer to 2006 Neurocomputing paper
-%OutputWeight=inv(eye(size(H,1))/C+H * H') * H * T';   % faster method 1 //refer to 2012 IEEE TSMC-B paper
-%implementation; one can set regularizaiton factor C properly in classification applications 
-OutputWeight=(eye(size(H,1))/C+H * H') \ H * T';      % faster method 2 //refer to 2012 IEEE TSMC-B paper
-%implementation; one can set regularizaiton factor C properly in classification applications
-
-%If you use faster methods or kernel method, PLEASE CITE in your paper properly: 
-
-%Guang-Bin Huang, Hongming Zhou, Xiaojian Ding, and Rui Zhang, "Extreme Learning Machine for Regression and Multi-Class Classification," submitted to IEEE Transactions on Pattern Analysis and Machine Intelligence, October 2010. 
-
-end_time_train=toc(start_time_train);%cputime;
-TrainingTime=end_time_train-start_time_train        %   Calculate CPU time (seconds) spent for training ELM
-
-%%%%%%%%%%% Calculate the training accuracy
-Y=(H' * OutputWeight)';                             %   Y: the actual output of the training data
-if Elm_Type == REGRESSION
-    TrainingAccuracy=sqrt(mse(T - Y));               %   Calculate training accuracy (RMSE) for regression case
-end
+%   Y is the actual output of the training data
+Y=(H' * OutputWeight)';                            
 clear H;
 
-%%%%%%%%%%% Calculate the output of testing input
-start_time_test=tic;%cputime;
-tempH_test=InputWeight*TV.P;
-clear TV.P;             %   Release input of testing data             
-ind=ones(1,NumberofTestingData);
-BiasMatrix=BiasofHiddenNeurons(:,ind);              %   Extend the bias matrix BiasofHiddenNeurons to match the demention of H
+%--- Calculate the output of testing input
+start_time_test=tic;
+tempH_test=InputWeight*Xt;
+ind=ones(1,Nt);
+%--- Extend the bias matrix Bhn to match the demention of H
+BiasMatrix=Bhn(:,ind); 
 tempH_test=tempH_test + BiasMatrix;
 switch lower(ActivationFunction)
-    case {'sig','sigmoid'}
+   case {'logsig','sigmoid'}
         %%%%%%%% Sigmoid 
         H_test = 1 ./ (1 + exp(-tempH_test));
-    case {'logsig','logsigmoid'}
+    case {'tansig'}
         %%%%%%%% Sigmoid 
-        H_test = 1 ./ (1 + exp(-tempH_test));
-    case {'sin','sine'}
-        %%%%%%%% Sine
-        H_test = sin(tempH_test);        
-    case {'hardlim'}
-        %%%%%%%% Hard Limit
-        H_test = hardlim(tempH_test);        
-    case {'tribas'}
-        %%%%%%%% Triangular basis function
-        H_test = tribas(tempH_test);        
-    case {'radbas'}
-        %%%%%%%% Radial basis function
-        H_test = radbas(tempH_test);  
-         case {'tansig'}
-        %%%%%%%% Radial basis function
         H_test = tansig(tempH_test);   
-        %%%%%%%% More activation functions can be added here        
 end
-TY=(H_test' * OutputWeight)';                       %   TY: the actual output of the testing data
+%--- TY is the actual output of the testing data
+TY=(H_test' * OutputWeight)';  
+TestingTime=toc(start_time_test);
+timeTe(ite)=TestingTime;
 
-TestingTime=toc(start_time_test);%cputime;
-%TestingTime=end_time_test-start_time_test           %   Calculate CPU time (seconds) spent by ELM predicting the whole testing data
-time(ite)=TestingTime;
-if Elm_Type == REGRESSION
-    TestingAccuracy=sqrt(mse(TV.T - TY));            %   Calculate testing accuracy (RMSE) for regression case
-end
-
+ %---  Calculate training & testing classification accuracy
 if Elm_Type == CLASSIFIER
-%%%%%%%%%% Calculate training & testing classification accuracy
-    MissClassificationRate_Training=0;
-    MissClassificationRate_Testing=0;
-
-    for i = 1 : size(T, 2)
-        [x, label_index_expected]=max(T(:,i));
-        [x, label_index_actual]=max(Y(:,i));
-        if label_index_actual~=label_index_expected
-            MissClassificationRate_Training=MissClassificationRate_Training+1;
-        end
+WrongGuessClass=0;    WrongGuessClassT=0;
+Yd_tr=Yd_train; Yd_te=Yd_test;
+%--- Calculate training classification accuracy
+    for i = 1 : size(Yd_tr, 2)
+        [x, i_Labeld]=max(Yd_tr(:,i));
+        [x, i_labela]=max(Y(:,i));
+            if i_labela~=i_Labeld
+                WrongGuessClass=WrongGuessClass+1;
+            end
     end
-    TrainingAccuracy=1-MissClassificationRate_Training/size(T,2);
-    for i = 1 : size(TV.T, 2)
-        [x, label_index_expected]=max(TV.T(:,i));
-        [x, label_index_actual]=max(TY(:,i));
-        if label_index_actual~=label_index_expected
-            MissClassificationRate_Testing=MissClassificationRate_Testing+1;
-        end
+    TrainingAccuracy(ite)=1-WrongGuessClass/size(Yd_tr,2);
+%--- Calculate testing classification accuracy
+    for i = 1 : size(Yd_te, 2)
+        [x, i_Labeld]=max(Yd_te(:,i));
+        [x, i_labela]=max(TY(:,i)); % TY: the actual output of the testing data
+            if i_labela~=i_Labeld
+                WrongGuessClassT=WrongGuessClassT+1;
+            end
     end
-    TestingAccuracy=1-MissClassificationRate_Testing/size(TV.T,2);  
-end
+    TestingAccuracy(ite)=1-WrongGuessClassT/size(Yd_te,2);
+end % Classifier_if
 ite=ite+1; %iteration increment
 end % while ite
-disp('------------- ELM ------------- ');
-disp('[  RegFac.   Nnode     TrAcc.       TeAcc.    TrTime  TeTime  ActivationFunc.  ]');
-XX=[           C     NumberofHiddenNeurons  TrainingAccuracy   TestingAccuracy       TrainingTime   mean(time) string(ActivationFunction)];
-disp(XX);
-end
+
+TrAcc=mean(TrainingAccuracy)
+TrTime=mean(timeTr)
+TeAcc=mean(TestingAccuracy)
+TeTime=mean(timeTe)
